a98a273016ebff1240b426c2b92324a90419c54a
L.Meijerink <lotta.meijerink@pacmed.nl>
2020-04-20 11:37:57 +0200

    adding docstrings to nn_ensemble

--- unc_classification_tab_data/models/nn_ensemble.py
+++ unc_classification_tab_data/models/nn_ensemble.py
@@ -4,28 +4,62 @@ import numpy as np
 
 
 class NNEnsemble:
-    def __init__(self, n_models, model_params):
+    """Wrapper class for an ensemble of neural networks.
+
+    Parameters
+    ----------
+    n_models: int
+        The number of ensemble members.
+    model_params: dict
+        The model parameters, see class MLP.
+    """
+
+    def __init__(self, n_models: int, model_params: dict):
         self.n_models = n_models
         self.model_params = model_params
         self.models = dict()
 
-    def train(self, X_train, y_train, X_val, y_val, training_params):
+    def train(self, X_train: np.ndarray, y_train: np.ndarray, X_val: np.ndarray, y_val: np.ndarray,
+              training_params: dict):
+        """Train all MLPs on the training data.
+
+        Parameters
+        ----------
+        X_train: np.ndarray
+            The training data
+        y_train: np.ndarray
+            The labels corresponding to the training data.
+        X_val: np.ndarray
+            The validation data
+        y_val: np.ndarray
+            The labels corresponding to the validation data.
+        training_params: dict
+            The parameters used for training, see class MLP.
+        """
         for i in range(self.n_models):
             mlp = MLP(self.model_params)
             mlp.train(X_train, y_train, X_val, y_val,
                       training_params)
             self.models[i] = mlp.model
 
-    def predict(self, X_test):
+    def predict_proba(self, X_test: np.ndarray) -> np.ndarray:
+        """Predict the probabilities p(y|X) by averaging the predictions of ensemble members.
+
+        Parameters
+        ----------
+        X_test: np.ndarray
+            The test data.
+
+        Returns
+        -------
+        type:np.ndarray
+            The predicted probabilities.
+        """
         predictions = []
         X_test_tensor = torch.tensor(X_test).float()
         for i in range(self.n_models):
             self.models[i].eval()
             predictions += [torch.sigmoid(
                 self.models[i](X_test_tensor)).detach().squeeze().numpy()]
-        predictions = np.mean(np.array(predictions), axis=0)
-        return predictions
-
-    def predict_proba(self, X_test):
-        mean_predictions = self.predict(X_test)
+        mean_predictions = np.mean(np.array(predictions), axis=0)
         return np.stack([1 - mean_predictions, mean_predictions], axis=1)
